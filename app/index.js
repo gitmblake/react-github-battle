81bdb65db2617be04ec59dadd25adcd383863952
Michael Blake <Michael Blake>
2016-09-22 19:50:30 +1000

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var HelloWorld = React.createClass({
+    render: function() {
+    return (
+            <div> Hello {this.props.name}</div>
+        )
+    }
+})
+
+ReactDOM.render(
+    <HelloWorld name="Michael" />,
+    document.getElementById("app")
+)
